51f613f99da5438a2196c4090723fe6e54b26521
nvckdev <nictro.cc@gmail.com>
2026-05-16 15:53:15 -0400

    Revert "Switch to Inter + Barlow Condensed typography"
    This reverts commit 60784b11c2ae0946108c2a410d9cefbf2fb7ef6f.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Inter, Barlow_Condensed } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const jakartaSans = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
-});
-
-const barlowCondensed = Barlow_Condensed({
-  variable: "--font-barlow",
-  subsets: ["latin"],
-  weight: ["700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${barlowCondensed.variable} dark`}>
+    <html lang="en" className={`${jakartaSans.variable} dark`}>
       <head>
         <link
           rel="stylesheet"
